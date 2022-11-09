40a0b5571de16ec3f7310bd47d61ebd797a5fc5f
anggris <ferdinandanggris@gmail.com>
2022-11-09 07:45:33 +0700

    add url on about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
         <h2 class="border-bottom">About Me</h2>
         <div class="profile d-flex align-items-center justify-item-between" style="">
             <div class="introduce p-3" >
-                <p>Hi, perkenalkan nama saya <span><a href="">Ferdinand Anggris Winarko</a></span>.Bisa dipanggil Anggris.</p>
+                <p>Hi, perkenalkan nama saya <span><a href="https://web.facebook.com/profile.php?id=100009318102112">Ferdinand Anggris Winarko</a></span>.Bisa dipanggil Anggris.</p>
                 <p>Saya membuat blog ini untuk sharing ilmu-ilmu yang saya punya, sekalian mengingat apa aja ilmu yang udah saya pelajari, biar gak mudah lupa😃.Karena ada sebuah nasihat yang berbunyi <q><i>Ajarkanlah ilmu yang baru kamu kuasai ke orang yang belum tahu, jika kamu berhasil, maka kamu sudah paham atas ilmu itu</i></q>.</p>
             </div>
         </div>
