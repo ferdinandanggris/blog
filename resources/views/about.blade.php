5927f24baea8beb67aabc06235f9218b53c8e2cb
ferdinandanggris <ferdinandanggris@gmail.com>
2022-11-12 17:58:55 +0700

    update primary color

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="container" style="height: 25em;">
-        <h2 class="border-bottom">About Me</h2>
+    <div class="container" style="min-height: 450px">
+        <h2 class="border-bottom text-primary">About Me</h2>
         <div class="profile d-flex align-items-center justify-item-between" style="">
-            <div class="introduce p-3" >
-                <p>Hi, perkenalkan nama saya <span><a href="https://web.facebook.com/profile.php?id=100009318102112">Ferdinand Anggris Winarko</a></span>.Bisa dipanggil Anggris.</p>
-                <p>Saya membuat blog ini untuk sharing ilmu-ilmu yang saya punya, sekalian mengingat apa aja ilmu yang udah saya pelajari, biar gak mudah lupa😃.Karena ada sebuah nasihat yang berbunyi <q><i>Ajarkanlah ilmu yang baru kamu kuasai ke orang yang belum tahu, jika kamu berhasil, maka kamu sudah paham atas ilmu itu</i></q>.</p>
+            <div class="introduce p-3">
+                <p>Hi, perkenalkan nama saya <span><a
+                            href="https://web.facebook.com/profile.php?id=100009318102112">Ferdinand Anggris
+                            Winarko</a></span>.Bisa dipanggil Anggris.</p>
+                <p>Saya membuat blog ini untuk sharing ilmu-ilmu yang saya punya, sekalian mengingat apa aja ilmu yang udah
+                    saya pelajari, biar gak mudah lupa😃.Karena ada sebuah nasihat yang berbunyi <q><i>Ajarkanlah ilmu yang
+                            baru kamu kuasai ke orang yang belum tahu, jika kamu berhasil, maka kamu sudah paham atas ilmu
+                            itu</i></q>.</p>
             </div>
         </div>
     </div>
